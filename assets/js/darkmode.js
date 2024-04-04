6dd75ad0565e315744d9084f0fb6c7b3d317695d
Slobodan Radosevic <slobodan.radosevic@coming.rs>
2024-04-04 12:46:55 +0200

    darkmode test

---
+++ assets/js/darkmode.js
@@ -0,0 +1,51 @@
+function toggleDarkMode() {
+    var darkModeEnabled = getCookie('darkModeEnabled');
+
+    // If dark mode is enabled, disable it
+    if (darkModeEnabled === 'true') {
+        setCookie('darkModeEnabled', 'false', 30); // Set cookie to expire in 30 days
+    } else { // Otherwise, enable dark mode
+        setCookie('darkModeEnabled', 'true', 30); // Set cookie to expire in 30 days
+    }
+
+    applyDarkMode();
+}
+
+// Apply dark mode based on cookie value
+function applyDarkMode() {
+    var darkModeEnabled = getCookie('darkModeEnabled');
+
+    if (darkModeEnabled === 'true') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+// Function to set cookie
+function setCookie(name, value, days) {
+    var expires = '';
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + date.toUTCString();
+    }
+    document.cookie = name + '=' + value + expires + '; path=/';
+}
+
+// Function to get cookie value by name
+function getCookie(name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// Apply dark mode on page load
+window.onload = function () {
+    applyDarkMode();
+};
